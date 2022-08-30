d660c8787d557b0393b0cfed809685cd9488f8d9
tbpassin <pub1@tompassin.net>
2022-08-30 08:41:32 -0400

    add dummy plugin for development purposes.

---
+++ gf4/plugins/test_plugin.py
@@ -0,0 +1,7 @@
+#@+leo-ver=5-thin
+#@+node:tom.20220829181057.1: * @file test_plugin.py
+#@@language python
+"""Developmental plugin - does nothing useful."""
+
+BUTTON_DEF  = ('Plugin Test', 'plugin-test', 'A dummy plugin for development')
+#@-leo
