86336805f22f5247706903aac4afc1ec388dbff7
tbpassin <pub1@tompassin.net>
2022-11-16 12:07:56 -0500

    Delete obsolete file.  Handle non-existent plotngr.

--- gf4/plugins/require_datasets.py
+++ gf4/plugins/require_datasets.py
@@ -2,10 +2,13 @@
 #@+node:tom.20220830184545.1: * @file require_datasets.py
 from AbstractPlotMgr import MAIN, BUFFER
 
-def needs_main(plotmgr):
+def needs_main(plotmgr = None):
     """Return True if X (MAIN) dataset exists, otherwise flash error message 
        and return False.
     """
+    if not plotmgr:
+        return False
+
     _main = plotmgr.stack[MAIN]
     if not (_main and any(_main.xdata)):
         msg = 'Missing Waveform'
@@ -14,10 +17,13 @@ def needs_main(plotmgr):
         return False
     return True
 
-def needs_main_buffer(plotmgr):
+def needs_main_buffer(plotmgr = None):
     """Return True if both the X and Y (MAIN and BUFFER) datasets exist,
        otherwise flash error message and return False.
     """
+    if not plotmgr:
+        return False
+
     _main = plotmgr.stack[MAIN]
     _buff = plotmgr.stack[BUFFER]
     if not (_main and any(_main.xdata) and
@@ -27,6 +33,7 @@ def needs_main_buffer(plotmgr):
         return False
     return True
 
+# Better names:
 has_main = needs_main
 has_main_buffer = needs_main_buffer
 #@-leo
