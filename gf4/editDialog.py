9495820d93e5f08d53ccd394502b101ec0bb210e
tbpassin <pub1@tompassin.net>
2023-02-03 00:17:58 -0500

    Adapt editDialog for unit testing.

--- gf4/editDialog.py
+++ gf4/editDialog.py
@@ -1,22 +1,23 @@
 #@+leo-ver=5-thin
-#@+node:tom.20211211170820.45: * @file editDialog.py
+#@+node:tom.20230203001637.1: * @file editDialog.py
 """A dialog with an editing pane, an accept button, and a cancel button.
 When closed, returns the text of the editing pane or an empty string.
 """
 # pylint: disable = consider-using-f-string
 #@+others
-#@+node:tom.20211211170820.46: ** Imports
+#@+node:tom.20230203001637.2: ** Imports
 #import Tkinter as Tk
 try:
     import Tkinter as Tk
 except:
     import tkinter as Tk
 
-#@+node:tom.20211211170820.47: ** class editDialog(Tk.Toplevel)
+unittesting = False
+#@+node:tom.20230203001637.3: ** class editDialog(Tk.Toplevel)
 class editDialog(Tk.Toplevel):
     # pylint: disable = too-many-ancestors
     #@+others
-    #@+node:tom.20211211170820.48: *3* editDialog.__init__
+    #@+node:tom.20230203001637.4: *3* editDialog.__init__
     def __init__(self, parent=None, title = None):
         Tk.Toplevel.__init__(self, parent)
 
@@ -43,19 +44,20 @@ class editDialog(Tk.Toplevel):
         Tk.Button(frm, text="Cancel", padx=5, command=self.cancel).pack()
         frm.pack(pady=20)
 
-        editor.focus_set()
-        self.wait_window(self)
+        if not unittesting:
+            editor.focus_set()
+            self.wait_window(self)
 
 
-    #@+node:tom.20211211170820.49: *3* editDialog.accept
+    #@+node:tom.20230203001637.5: *3* editDialog.accept
     def accept(self, event=None):
-        self.result = self.editor.get('1.0', Tk.END)
+        self.result = self.editor.get('1.0', Tk.END)[:-1]  # Strip added '\n'
         if self.parent:
             self.parent.focus_set()
         self.window.destroy()
 
 
-    #@+node:tom.20211211170820.50: *3* editDialog.cancel
+    #@+node:tom.20230203001637.6: *3* editDialog.cancel
     def cancel(self, event=None):
         # put focus back to the parent window
         self.result = ''
@@ -64,11 +66,17 @@ class editDialog(Tk.Toplevel):
         return ''
 
     #@-others
-#@+node:tom.20211211170820.51: ** if __name__ == '__main__':
+#@+node:tom.20230203001637.7: ** if __name__ == '__main__':
 if __name__ == '__main__':
-    editDialog('Data Input')
+    unittesting = True
+    PHRASE = 'This is a test'
+    dia = editDialog(None, 'Test editDialog')
+    unittesting = True
+    ed = dia.editor
+    ed.insert('1.0', PHRASE)
+    dia.accept()
 
-    Tk.mainloop()
+    print(('fail', 'pass')[PHRASE == dia.result])
 #@-others
 #@@language python
 #@@tabwidth -4
