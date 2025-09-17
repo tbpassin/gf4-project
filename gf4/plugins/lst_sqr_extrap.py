e16437a18fd7a323d78868f2f36864d057d33aaf
tbpassin <com1@tompassin.net>
2025-09-17 16:56:47 -0400

    Make the least-squares extrapolation plugin position into the "Fit" group.

--- gf4/plugins/lst_sqr_extrap.py
+++ gf4/plugins/lst_sqr_extrap.py
@@ -9,7 +9,9 @@ from help_cmds import HELPTEXT
 
 BUTTON_DEF = ('Lst Sqr + Extrap', 'lst-sqr-extrap',
                'Least squares fit of [X] Data With Nth-Order Polynomial and Optional Extrapolation')
-OVERRIDE = False
+OVERRIDE = True
+OWNER_GROUP = 'CURVE_FIT_BUTTONS'
+
 plotmgr = None
 # mypy fails some of the following because plotmgr is None here,
 # but at runtime it will be an instance of an actual PlotManager.
