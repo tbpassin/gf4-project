6e5758ce8169be7740658d7594011497dce58657
tbpassin <com1@tompassin.net>
2025-11-09 00:30:28 -0500

    Make sure not to use lags longer than the data length. Improve the
    extended help message. Move declarations to a named section.

--- gf4/plugins/ljung_box.py
+++ gf4/plugins/ljung_box.py
@@ -17,16 +17,25 @@ from AbstractPlotMgr import MAIN
 from .require_datasets import has_main
 from help_cmds import HELPTEXT
 
-BUTTON_DEF  = ('Ljung-Box', 'ljung-box',
-               'Assess randomness of [X] by looking at autocorrelation coefficients using the Ljung-Box test ')
+#@+<< Declarations >>
+#@+node:tom.20251108211005.1: ** << Declarations >>
 OVERRIDE = False  # Override default location.
 GRAY_LOW, GRAY_HI = 0.01, 0.08
 BOOL_WORDS = {True: 'yes', False: 'no'}
 
-HELPTEXT['ljung-box'] = """
-Ljung-Box test.
-"""
+HELPTEXT['ljung-box'] = """\
+Ljung-Box test: Check autocorrelations at several lags.  Report the autocorrelations and their p-values (assuming the data is random).
+a p-value less than 0.05 is likely to indicate non-randomness.
+Very low p-values are highly unlikely for random data. One or more
+values near 0.05 may indicate an uncertain or ambiguous result.
 
+See https://en.wikipedia.org/wiki/Ljung%E2%80%93Box_test
+"""
+LAGS = [1,2,5,10,20,30]
+#@-<< Declarations >>
+BUTTON_DEF  = ('Ljung-Box', 'ljung-box',
+               'Assess randomness of [X] by looking at autocorrelation' 
+               'coefficients using the Ljung-Box test ')
 plotmgr = None
 
 # plotmgr will have been injected into the module by the time this is called
@@ -37,11 +46,17 @@ def proc():
     d1 = plotmgr.stack[MAIN].copy()
     ydata = d1.ydata
 
-    lj = sm.stats.acorr_ljungbox
-    result = lj(ydata, (1,2,5,10,20,30))
-    print(result)
-    pvalue = result['lb_pvalue']
+    # Highest lag must be less than data length
+    lags = LAGS[:]
+    while len(ydata) < max(lags) + 1:
+        lags = lags[:-1]
 
+    lj = sm.stats.acorr_ljungbox
+    result = lj(ydata, lags)
+    msg = str(result).replace('lb_', '').replace('pvalue', '    pvalue')
+    print(msg)
+
+    pvalue = result['lb_pvalue']
     is_random = None
     is_gray = bool(pvalue.between(GRAY_LOW, GRAY_HI, 
                      inclusive="both").any())
