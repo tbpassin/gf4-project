585840a0c271c9a6c581a998380e298d3d5e292d
tbpassin <com1@tompassin.net>
2025-11-14 00:31:14 -0500

    Correct logic to make sure if a max value is found at the lower or upper limits of the span
    that it is not reported as a peak. Draw vertical marker line at the peak's location.

--- gf4/plugins/find_peak.py
+++ gf4/plugins/find_peak.py
@@ -1,6 +1,6 @@
 #@+leo-ver=5-thin
 #@+node:tom.20220905122752.1: * @file find_peak.py
-"""Find a peak within the span denoted by dragging the mouse.
+"""Find a positive peak within the span denoted by dragging the mouse.
 
 X-axis points must be ordered. The peak will be the point
 with the highest value in the span. The peak will be marked
@@ -11,7 +11,7 @@ from numpy import searchsorted, argmax
 from AbstractPlotMgr import MAIN
 from .require_datasets import has_main
 
-BUTTON_DEF  = ('Find Peak', 'find-peak', 'Find the peak near the given x-axis location. Drag mouse across peak after clicking this button.')
+BUTTON_DEF  = ('Find Peak', 'find-peak', 'Find the (positive-valued) peak near the selected x-axis span. Drag mouse across peak after clicking this button.')
 OVERRIDE = False
 
 # plotmgr will have been injected into the module by the time this is called
@@ -27,23 +27,26 @@ def getSpan(xmin, xmax):
 
     span = (xmin, xmax)
     start, end = searchsorted(_x, span)
+
     segment = _y[start:end]
     if len(segment) == 0:
-        plotmgr.announce(f'Computed improper bounds: ){start:0.4f}, {end:0.4f}')
+        plotmgr.announce(f'Computed improper bounds: {start:0.4f}, {end:0.4f}')
         plotmgr.flashit()
         return
 
     peak = max(segment)
     peak_index = start + argmax(segment)
     x_peak = _x[peak_index]
+
     # Might have max at end of segment - if so, it's not a peak
-    if (_y[peak_index - 1] > peak) or (_y[peak_index + 1] > peak):
-        plotmgr.announce(f'No peak found in ({_x[start]:0.4f}, {_x[end]:0.4f})')
+    if peak_index == start  or peak_index == end - 1:
+        plotmgr.announce(f'No peak found in ({_x[start]:0.4f}, {_x[end - 1]:0.4f})')
         return
 
-    # plotmgr.timehack(x_peak)
-    plotmgr.announce(
-        f'Peak: {peak:0.4f} at {x_peak:0.4f})')
+    # Mark peak location
+    plotmgr.timehack(x_peak)
+
+    plotmgr.announce(f'Peak: {peak:0.4f} at {x_peak:0.4f})')
 
 # All plugins must define a proc()
 def proc():
